45f43f69effa95903bb339427a326e44f0c0b946
KenjiV14 <61467794+KenjiV14@users.noreply.github.com>
2021-10-19 19:57:49 -0500

    Update rpsGame.c

--- rpsGame.c
+++ rpsGame.c
@@ -1,2 +1,23 @@
 #include "rpsGame.h"
 
+void sndInt(int socket, int data){
+    int err;
+    char dataString[(int)((ceil(log10(data))+1)*sizeof(char))];
+
+    sprintf(dataString, "%d", data);
+    err = send(socket, dataString, (sizeof dataString / sizeof(char)), 0);
+    if(err == -1){
+        perror("send Failed\n");
+        exit(1);
+    }
+}
+
+void sndString(int socket, char* data){
+    int err;
+
+    err = send(socket, data, strlen(data)+1, 0);
+    if(err == -1){
+        perror("send Failed.\n");
+        exit(2);
+    }
+}
